5fd6ce8c2962b0c125fd02c9957bcf5bc96e57d8
Tomasen <tomasen@gmail.com>
2016-02-04 04:23:46 +0800

    use ./ as confdir default value

--- main.go
+++ main.go
@@ -2,20 +2,14 @@ package main
 
 import (
 	"flag"
-	"log"
-	"os"
 
 	"github.com/tomasen/dnsmasq-china-list/src"
 )
 
 func main() {
 
-	pwd, err := os.Getwd()
-	if err != nil {
-		log.Println("get pwd error:", err)
-	}
 	var logfile = flag.String("logfile", "/var/log/dnsmasq.log", "dnsmasq query log file to be analyzed")
-	var confDir = flag.String("confdir", pwd, "current dnsmasq conf dir")
+	var confDir = flag.String("confdir", "./", "current dnsmasq conf dir")
 	var output = flag.String("output", "accelerated-domains2.conf", "output config filename")
 
 	flag.Parse()
