417f9f12689db81b5b194481f47866a27847d333
Tomasen <tomasen@gmail.com>
2016-02-16 22:18:42 +0800

    update china dns list

--- src/worker.go
+++ src/worker.go
@@ -14,7 +14,7 @@ import (
 var (
 	// ']: reply d.dropbox.com is '
 	logEntry = regexp.MustCompile(`\]\:\ reply\ ([a-zA-z0-9\-\.]+) is`)
-	chinaNS  = regexp.MustCompile(`(qq.com|dnspod|360safe|sina|\.dnsv|baidu|lecloud|5173|tudoudns|letvlb|qingcdn|xinhuanet|youku|yodao|duowanns|sogou|alidns|kingsoft|aliyun|xunlei|alipay|ourdvs|taobao|uc\.cn|hichina|iqiyi|chinacache|ccgslb|\.cn\.|nease|aoyou365|sohu)`)
+	chinaNS  = regexp.MustCompile(`(qq.com|dnspod|360safe|sina|baidu|lecloud|5173|tudoudns|letvlb|qingcdn|xinhuanet|youku|yodao|duowanns|sogou|alidns|kingsoft|aliyun|xunlei|alipay|ourdvs|taobao|uc\.cn|hichina|iqiyi|chinacache|ccgslb|\.cn\.|nease|aoyou365|sohu)`)
 )
 
 // ReadDNSMasqLogfile analyz dnsmasq log file
